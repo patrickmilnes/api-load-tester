e1908a42582959f33186301b7ae4628fca0b0f79
patrickmilnes <pjamilnes@gmail.com>
2023-04-17 16:33:10 +0100

    feat: Added action for building

---
+++ src/runner_results.rs
@@ -0,0 +1,9 @@
+use crate::run_result::RunResults;
+
+pub fn print_results_pretty(results: RunResults) {
+    todo!()
+}
+
+pub fn save_to_file(results: RunResults) {
+    todo!("Need to research how to create and save to file")
+}
